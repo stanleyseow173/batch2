c7f135526a855920cea17d35110651ca06a38412
stanleyseow173 <seowszehowestanley@gmail.com>
2023-03-21 23:31:05 +0800

    first commit

---
+++ src/test/java/sg/edu/nus/iss/batch2/Batch2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.batch2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Batch2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
